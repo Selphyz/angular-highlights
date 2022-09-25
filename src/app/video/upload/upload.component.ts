bc143338f0d48a40e5229b4b42ed0a50880bb6cf
Carlos <carlos.dom13@hotmail.com>
2022-09-25 02:09:18 +0200

    file upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import firebase from 'firebase/compat/app';
+import {ClipService} from "../../services/clip.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements OnDestroy {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait while your clip is uploaded...';
+  inSubmission = false;
+  percentage = 0;
+  showPercetage = false;
+  user: firebase.User | null = null;
+  task?: AngularFireUploadTask
   title = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
@@ -19,14 +32,21 @@ export class UploadComponent implements OnInit {
   uploadForm = new FormGroup({
     title: this.title
   })
-  constructor(private storage: AngularFireStorage) { }
-
-  ngOnInit(): void {
+  constructor(private storage: AngularFireStorage,
+      private auth: AngularFireAuth,
+      private clipService: ClipService,
+      private routerService: Router) {
+    auth.user.subscribe(user => this.user = user)
   }
 
-  storeFile($event: DragEvent) {
+  ngOnDestroy(): void {
+        this.task?.cancel();
+  }
+
+  storeFile($event: Event) {
     this.isDragover = false;
-    this.file = $event.dataTransfer?.files.item(0) ?? null;
+    this.file = ($event as DragEvent).dataTransfer ? ($event as DragEvent).dataTransfer?.files.item(0) ?? null :
+    ($event.target as HTMLInputElement).files?.item(0) ?? null;
     if(!this.file || this.file.type !== 'video/mp4'){
       return;
     }
@@ -34,8 +54,50 @@ export class UploadComponent implements OnInit {
     this.nextStep = true;
   }
   uploadFile() {
+    this.uploadForm.disable();
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait while your clip is uploaded...';
+    this.inSubmission = true;
+    this.showPercetage = true;
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}`;
-    this.storage.upload(clipPath, this.file);
+    this.task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
+    this.task.percentageChanges().subscribe(progress => {
+      this.percentage = progress as number / 100
+    })
+    this.task.snapshotChanges().pipe(
+      last(),
+      switchMap(() => clipRef.getDownloadURL())
+      ).subscribe({
+      next: async (url) => {
+        const clip = {
+          uid: this.user?.uid as string,
+          displayName: this.user?.displayName as string,
+          title: this.title.value as string,
+          fileName: `${clipFileName}.mp4`,
+          url,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }
+        const clipDocRef = await this.clipService.createClip(clip);
+        this.alertMsg = 'green';
+        this.alertMsg = 'Success! File uploaded';
+        this.showPercetage = false;
+        setTimeout(()=>{
+          this.routerService.navigate([
+            'clip', clipDocRef.id
+          ])
+        }, 1000)
+      },
+      error: (error) => {
+        this.uploadForm.enable();
+        this.alertColor = 'red';
+        this.alertMsg = 'Upload failed! Try again later';
+        this.inSubmission = true;
+        this.showPercetage = false;
+        console.error(error)
+      }
+    })
   }
 }
